7e6e2256acca7c925bb889e33d529f3775539e97
Philip Dimaano <pdimaano7@gmail.com>
2024-02-15 12:19:05 -0800

    changed id to strings to comply with typing

--- productiv/src/App.tsx
+++ productiv/src/App.tsx
@@ -12,19 +12,19 @@ import "./App.css";
 
 const initialTodos: TodoInterface[] = [
   {
-    id: 1,
+    id: "1",
     title: "Code!",
     description: "Write some code",
     priority: 2,
   },
   {
-    id: 2,
+    id: "2",
     title: "Make dinner",
     description: "Cook something healthy",
     priority: 1,
   },
   {
-    id: 3,
+    id: "3",
     title: "Go to bed",
     description: "In bed by 11:15",
     priority: 3,
