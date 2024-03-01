693a115019a92bc0afe5fc2924926281a042948d
Philip Dimaano <pdimaano7@gmail.com>
2024-03-01 14:18:37 -0800

    new import statement for interface

--- productiv/src/QuoteApp.tsx
+++ productiv/src/QuoteApp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import { QuoteInterface } from "./interfaces";
 import QuoteDisplay from "./QuoteDisplay";
 
 /** Component that retrieves and sets an inspirational quote,
