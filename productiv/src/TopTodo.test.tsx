49b5d489b2f7d9261962cd6c770baeccf0af22fb
Philip Dimaano <pdimaano7@gmail.com>
2024-06-12 17:50:44 -0700

    remove test todos

--- productiv/src/TopTodo.test.tsx
+++ productiv/src/TopTodo.test.tsx
@@ -1,23 +1,6 @@
 import { render } from "@testing-library/react";
 import TopTodo from "./TopTodo";
 
-
-const emptyTodos = [];
-
-const todo1 = {
-  id: 1,
-  title: "Code!",
-  description: "Write important code",
-  priority: 2,
-};
-
-const todo2 = {
-  id: 1,
-  title: "Code again!",
-  description: "Write less-important code",
-  priority: 11,
-}
-
 const populatedTodos = [todo1, todo2];
 
 it("returns null when todos list is empty", function () {
