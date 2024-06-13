77d65137cd9c8b503d37766b7687b961b93cd15d
Philip Dimaano <pdimaano7@gmail.com>
2024-06-12 17:52:07 -0700

    change entry for empty todo

--- productiv/src/TopTodo.test.tsx
+++ productiv/src/TopTodo.test.tsx
@@ -2,6 +2,6 @@ import { render } from "@testing-library/react";
 import TopTodo from "./TopTodo";
 
 it("returns null when todos list is empty", function () {
-  const result = render(<TopTodo todos={emptyTodos} />);
+  const result = render(<TopTodo todos={[]} />);
   expect(result.queryByText("priority")).not.toBeInTheDocument();
 });
