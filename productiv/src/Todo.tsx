776595a1a6ac58a7c926031f2f8fd4164b369649
Philip Dimaano <pdimaano7@gmail.com>
2024-07-17 13:59:49 -0700

    changed name for ranking system

--- productiv/src/Todo.tsx
+++ productiv/src/Todo.tsx
@@ -12,7 +12,7 @@ import { TodoInterface } from "./interfaces";
 function Todo({ todo }: { todo: TodoInterface}) {
   return (
       <div className="Todo">
-        <div><b>{todo.title}</b> <small>(priority: {todo.priority})</small></div>
+        <div><b>{todo.title}</b> <small>(Ranking: {todo.priority})</small></div>
         <div><small>{todo.description}</small></div>
       </div>
   );
